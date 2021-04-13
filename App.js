fa636160b225fe9cd6a4ba831c1a46e235a7dfc0
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-04-13 12:08:15 +0300

    a closer look at the text component

--- App.js
+++ App.js
@@ -16,8 +16,8 @@ const fetchFonts = () => {
 };
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [guessRounds, setGuessRounds] = useState(0);
+  const [userNumber, setUserNumber] = useState(20);
+  const [guessRounds, setGuessRounds] = useState(5);
   const [dataLoaded, setDataLoaded] =  useState(false);
 
   if (!dataLoaded) {
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
+import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
     return (
@@ -11,12 +12,17 @@ const GameOverScreen = props => {
             <View style={styles.imageContainer}>
                 <Image 
                     source={require('../assets/success.png')} 
+                    //source={{uri: 'https://i.pinimg.com/originals/3d/40/de/3d40dea06e013013e0015537dc2f231c.jpg'}} 
                     style={styles.image}
                     resizeMode="cover"    
                 />
             </View>
-            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-            <BodyText>Number was: {props.userNumber}</BodyText>
+            <View style={styles.resultContainer}>
+                <BodyText style={styles.resultText}>
+                    Your phone needed <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess the 
+                    number <Text style={styles.highlight}>{props.userNumber}</Text>
+                </BodyText>
+            </View>
             <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     );
@@ -40,6 +46,18 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: '100%'
+    },
+    resultContainer: {
+        marginHorizontal: 30,
+        marginVertical: 15
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
+    },
+    highlight: {
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold',
     }
 });
 
